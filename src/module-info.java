e5bb9211d73ad57f371baeda5b2433ee996776e2
juancamilobonet2 <j.bonet@uniandes.edu.co>
2021-11-24 09:01:20 -0500

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module taller6_Libreria {
+	exports uniandes.dpoo.taller1.modelo;
+	exports uniandes.dpoo.taller1.interfaz;
+
+	requires com.formdev.flatlaf;
+	requires java.desktop;
+	requires org.junit.jupiter.api;
+}
